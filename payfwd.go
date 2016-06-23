3882106ef458f0fc7968a6635051302759ef7484
Josh Cincinnati <acityinohio@users.noreply.github.com>
2016-06-23 00:27:36 +0000

    add payfwd page method

--- payfwd.go
+++ payfwd.go
@@ -1,5 +1,7 @@
 package gobcy
 
+import "strconv"
+
 //CreatePayFwd creates a new PayFwd forwarding
 //request associated with your API.Token, and
 //returns a PayFwd with a BlockCypher-assigned id.
@@ -16,6 +18,19 @@ func (api *API) CreatePayFwd(payment PayFwd) (result PayFwd, err error) {
 //associated with your API.Token.
 func (api *API) ListPayFwds() (payments []PayFwd, err error) {
 	u, err := api.buildURL("/payments", nil)
+	if err != nil {
+		return
+	}
+	err = getResponse(u, &payments)
+	return
+}
+
+//ListPayFwdsPage returns a PayFwds slice
+//associated with your API.Token, starting at the start index.
+//Useful for paging past the 200 payment forward limit.
+func (api *API) ListPayFwdsPage(start int) (payments []PayFwd, err error) {
+	params := map[string]string{"start": strconv.Itoa(start)}
+	u, err := api.buildURL("/payments", params)
 	if err != nil {
 		return
 	}
